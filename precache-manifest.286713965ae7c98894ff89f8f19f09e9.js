669c2b3665878c09086ce371250b27df8f1b2cf2
BKirilov <bkirilov2000@gmail.com>
2020-08-27 13:36:14 +0300

    Updates

---
+++ precache-manifest.286713965ae7c98894ff89f8f19f09e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d21dd996b094c5a475e569e695970ee",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "507ca469b7c8d9359254",
+    "url": "/Pomodoro-Clock/static/css/main.992cea11.chunk.css"
+  },
+  {
+    "revision": "94ca0ba0984f72429f47",
+    "url": "/Pomodoro-Clock/static/js/2.b18c1b52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.b18c1b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507ca469b7c8d9359254",
+    "url": "/Pomodoro-Clock/static/js/main.ed40737d.chunk.js"
+  },
+  {
+    "revision": "4ccfd468b853680e7d1a",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.46c75fa3.js"
+  }
+]);
